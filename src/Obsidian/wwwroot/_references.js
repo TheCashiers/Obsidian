28ae385030a49bf26c32e5ffac5cc2f140b7aa47
Cubelrti <tankrity@gmail.com>
2016-07-07 10:51:46 +0800

    Revert "Add React"
    This reverts commit ba7542ac8d716c3e51b98fbb127f5e5fbde5c5c4.

--- src/Obsidian/wwwroot/_references.js
+++ src/Obsidian/wwwroot/_references.js
@@ -4,5 +4,3 @@
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
-/// <reference path="lib/react/react.js" />
-/// <reference path="lib/react/react-dom.js" />
