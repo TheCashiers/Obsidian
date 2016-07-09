197df7de520969d231ee0aadb6b50a55fd143483
Henry Chu <HenryTech@outlook.com>
2016-07-09 17:08:25 +0800

    Added ManageCotroller and views mentioned in #2

---
+++ src/Obsidian/Controllers/ManageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Obsidian.Controllers
+{
+    public class ManageController : Controller
+    {
+        public IActionResult Index()        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
