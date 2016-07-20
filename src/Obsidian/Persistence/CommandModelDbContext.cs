2a9cdf88b84e5fca1adf0eba6855c1aa318732b0
Henry Chu <HenryTech@outlook.com>
2016-07-20 14:07:59 +0800

    added db contexts

---
+++ src/Obsidian/Persistence/CommandModelDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Obsidian.Domain;
+
+namespace Obsidian.Persistence
+{
+    public class CommandModelDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instence of <see cref="CommandModelDbContext"/>.
+        /// </summary>
+        /// <param name="options"></param>
+        public CommandModelDbContext(DbContextOptions<CommandModelDbContext> options)
+            : base(options)
+        {
+            Users = base.Set<User>();
+        }
+
+        /// <summary>
+        /// Used to query and save instences of <see cref="User"/>.
+        /// </summary>
+        public DbSet<User> Users { get; }
+    }
+}
---
+++ src/Obsidian/Persistence/QueryModelDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Obsidian.QueryModels;
+
+namespace Obsidian.Persistence
+{
+    public class QueryModelDbContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instence of <see cref="QueryModelDbContext"/>.
+        /// </summary>
+        /// <param name="options"></param>
+        public QueryModelDbContext(DbContextOptions<QueryModelDbContext> options)
+            :base(options)
+        {
+            Users = base.Set<User>();
+        }
+
+        /// <summary>
+        /// Used to query instences of <see cref="User"/>.
+        /// </summary>
+        public DbSet<User> Users { get; }
+    }
+}
