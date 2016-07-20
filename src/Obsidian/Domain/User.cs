a71a6813e0ac75f2000cd8a88922aee51b3047e0
Henry Chu <HenryTech@outlook.com>
2016-07-20 13:56:47 +0800

    added user class

---
+++ src/Obsidian/Domain/User.cs
@@ -0,0 +1,42 @@
+﻿using Shared;
+using System;
+
+namespace Obsidian.Domain
+{
+    public class User
+    {
+        /// <summary>
+        /// Initializes a new instence of <see cref="User"/>.
+        /// </summary>
+        /// <remarks>
+        /// Default constructor fot O/RM.
+        /// </remarks>
+        public User()
+        {
+        }
+
+        #region Props
+
+        /// <summary>
+        /// Represents the unique identifier of the user.
+        /// </summary>
+        public Guid Id { get; }
+
+        /// <summary>
+        /// Represents the username used to login.
+        /// </summary>
+        public string UserName { get; }
+
+        /// <summary>
+        /// Represents the display name of the <see cref="User"/> .
+        /// </summary>
+        public string DisplayName { get; }
+
+        /// <summary>
+        /// Represents the Gender of the <see cref="User"/>.
+        /// </summary>
+        public Gender Gender { get; }
+
+        #endregion Props
+    }
+}
---
+++ src/Obsidian/Shared/Gender.cs
@@ -0,0 +1,23 @@
+﻿namespace Shared
+{
+    /// <summary>
+    /// Represents the gender of a <see cref="Domain.User"/>.
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// It a secret.
+        /// </summary>
+        Privacy = 0,
+
+        /// <summary>
+        /// Male.
+        /// </summary>
+        Male = 1,
+
+        /// <summary>
+        /// Female
+        /// </summary>
+        Female = 2
+    }
+}
