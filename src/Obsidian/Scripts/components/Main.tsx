7d01305cb8dd065b3322d3d1a4254aef8e2122b6
Cubelrti <tankrity@gmail.com>
2016-07-30 13:38:54 +0800

    fix some layout bugs

--- src/Obsidian/Scripts/components/Main.tsx
+++ src/Obsidian/Scripts/components/Main.tsx
@@ -3,7 +3,38 @@
 // https://github.com/Microsoft/TypeScript/wiki/JSX
 import * as React from "react";
 import { PortalHeader, PortalSidebar } from "./PortalElements"
+var fixLayout = function () {
+    //Get window height and the wrapper height
+    var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
+    var window_height = $(window).height();
+    var sidebar_height = $(".sidebar").height();
+    //Set the min-height of the content and sidebar based on the
+    //the height of the document.
+    if ($("body").hasClass("fixed")) {
+        $(".content-wrapper, .right-side").css('min-height', window_height - $('.main-footer').outerHeight());
+    } else {
+        var postSetWidth;
+        if (window_height >= sidebar_height) {
+            $(".content-wrapper, .right-side").css('min-height', window_height - neg);
+            postSetWidth = window_height - neg;
+        } else {
+            $(".content-wrapper, .right-side").css('min-height', sidebar_height);
+            postSetWidth = sidebar_height;
+        }
+
+        //Fix for the control sidebar height
+        var controlSidebar = $(".control-sidebar");
+        if (typeof controlSidebar !== "undefined") {
+            if (controlSidebar.height() > postSetWidth)
+                $(".content-wrapper, .right-side").css('min-height', controlSidebar.height());
+        }
+
+    }
+};
 export let Main = React.createClass({
+    componentDidUpdate: () => {
+        fixLayout();
+    },
     render: function () {
         return (
             <div className="wrapper skin-blue">
