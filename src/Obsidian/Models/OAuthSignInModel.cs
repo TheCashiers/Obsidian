580837bf65a738a2f1e8a2f79daf1d24d9f16895
Henry Chu <HenryTech@outlook.com>
2016-08-08 23:15:17 +0800

    fixed display name for OAuth20 SignIn Model

--- src/Obsidian/Models/OAuthSignInModel.cs
+++ src/Obsidian/Models/OAuthSignInModel.cs
@@ -1,10 +1,18 @@
-﻿namespace Obsidian.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Obsidian.Models
 {
     public class OAuthSignInModel
     {
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
+
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
         public bool RememberMe { get; set; }
+
         public string ProtectedOAuthContext { get; set; }
     }
 }
