80886bfcd1ff56ac4a657775a5b309951761c668
SamLangTen <samlangten@outlook.com>
2016-08-10 12:01:57 +0800

    added mongodb driver code to repository

--- src/Obsidian.Persistence/Repositories/ClientRepository.cs
+++ src/Obsidian.Persistence/Repositories/ClientRepository.cs
@@ -5,39 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Obsidian.Domain;
 using Microsoft.EntityFrameworkCore;
-
+using MongoDB.Driver;
 namespace Obsidian.Persistence.Repositories
 {
     public class ClientRepository : IClientRepository
     {
-        private readonly CommandModelDbContext _dbContext;
-
-        public ClientRepository(CommandModelDbContext ctx)
+        private readonly MongoClient client;
+        private readonly IMongoDatabase database;
+        private readonly IMongoCollection<Client> collection;
+        public ClientRepository()
         {
-            _dbContext = ctx;
+            client = new MongoClient("mongodb://localhost:27017");
+            database = client.GetDatabase("Obsidian");
+            collection = database.GetCollection<Client>("Client");
         }
 
-        public async Task AddAsync(Client aggregate)
-        {
-            _dbContext.Clients.Add(aggregate);
-            await _dbContext.SaveChangesAsync();
-        }
+        public async Task AddAsync(Client aggregate) => await collection.InsertOneAsync(aggregate);
 
-        public Task DeleteAsync(Client aggregate)
-        {
-            throw new NotImplementedException();
-        }
+        public Task DeleteAsync(Client aggregate) => collection.DeleteOneAsync(Builders<Client>.Filter.Eq("id", aggregate.Id));
 
-        public Task<Client> FindByIdAsync(Guid id) => _dbContext.Clients.AsNoTracking().SingleOrDefaultAsync(c => c.Id == id);
+        public Task<Client> FindByIdAsync(Guid id) => collection.Find(Builders<Client>.Filter.Eq("id", id)).FirstOrDefaultAsync();
 
-        public Task<IQueryable<Client>> QueryAllAsync()
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task SaveAsync(Client aggregate)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<IQueryable<Client>> QueryAllAsync() => new Task<IQueryable<Client>>(() => collection.AsQueryable().AsQueryable());
+
+        public Task SaveAsync(Client aggregate) => collection.ReplaceOneAsync(Builders<Client>.Filter.Eq("id", aggregate.Id), aggregate);
     }
 }
