1cb3aad8c58fbdf1c6c751e4737bdbb23fe97697
Cubelrti <tankrity@gmail.com>
2016-08-18 22:34:42 +0800

    Revert "fix routing for oauth page"
    This reverts commit b55248d7773f0c5d2c4b42a1a15fb829d1119ce6.

--- src/Obsidian/Scripts/components/SignIn.tsx
+++ src/Obsidian/Scripts/components/SignIn.tsx
@@ -3,11 +3,13 @@ export const SignIn = (props) => (
     <div className="main fullscreen">
 
     <div className="bg cover text-padding left-div">
+        @*Left Section*@
         <h1>Obsidian</h1>
         <h3>One-stop Authenication Solution</h3>
     </div>
     <div className="white form form-padding">
         <form className="text-padding">
+            @*Right Section*@
             <div className="form-group">
                 <label htmlFor="UserName" className="control-label"></label>
                 <input htmlFor="UserName" className="form-control" type="text" placeholder="Enter your E-mail address" required/>
--- src/Obsidian/Scripts/configs/OAuthRoutes.tsx
+++ src/Obsidian/Scripts/configs/OAuthRoutes.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 
 import { Router, Route, IndexRoute, createMemoryHistory  } from "react-router";
-import { SignInContainer } from "../containers/SignInContainer";
+import { PortalContentWrapper } from "../components/PortalContentWrapper";
 import { UserManagementContainer } from "../containers/UserManagementContainer";
+import { Portal } from "../components/Portal";
 import { UserCreationContainer } from "../containers/UserCreationContainer"
 
 export const routes = (
     <Router history={ createMemoryHistory() }>
-        <Route path="/">
-            <IndexRoute component={ SignInContainer }/>
+        <Route path="/oauth">
+            <IndexRoute component={ Portal }/>
+            <Route path="/oauth/signin" component={ UserManagementContainer } />
+            <Route path="/manage/users/create" component={ UserCreationContainer } />
         </Route>
     </Router>
     
