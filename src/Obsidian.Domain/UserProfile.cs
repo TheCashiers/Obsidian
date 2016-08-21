73e9a5d95a95558f960e4e9cee7f22432a0d2281
SamLangTen <samlangten@outlook.com>
2016-08-21 16:07:51 +0800

    make tests in domain pass

--- src/Obsidian.Domain/UserProfile.cs
+++ src/Obsidian.Domain/UserProfile.cs
@@ -3,11 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace Obsidian.Domain
 {
     public class UserProfile
     {
+
         /// <summary>
         /// Represents the display name of a <see cref="User"/>.
         /// </summary>
@@ -18,10 +19,24 @@ namespace Obsidian.Domain
         /// </summary>
         public Gender Gender { get; set; }
 
+        private string _EmailAddress = null;
         /// <summary>
         /// Represents the Email address of a <see cref="User"/>.
         /// </summary>
-        public string EmailAddress { get; set; }
+        public string EmailAddress
+        {
+            get
+            {
+                return _EmailAddress;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+                if (string.IsNullOrWhiteSpace(value)) throw new ArgumentNullException(nameof(value));
+                if (new EmailAddressAttribute().IsValid(value) == false) throw new ArgumentException("", nameof(value));
+                _EmailAddress = value;
+            }
+        }
 
     }
 }
