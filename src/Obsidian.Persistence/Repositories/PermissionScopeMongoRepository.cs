8d81ccaf00133736e2a1c361b74131643d9a0495
SamLangTen <samlangten@outlook.com>
2016-08-21 15:54:09 +0800

    make some easy tests green first

--- src/Obsidian.Persistence/Repositories/PermissionScopeMongoRepository.cs
+++ src/Obsidian.Persistence/Repositories/PermissionScopeMongoRepository.cs
@@ -10,39 +10,48 @@ namespace Obsidian.Persistence.Repositories
 {
     public class PermissionScopeMongoRepository : IPermissionScopeRepository
     {
+        private readonly IMongoCollection<PermissionScope> _collection;
         public PermissionScopeMongoRepository(IMongoDatabase database)
         {
-
+            _collection = database.GetCollection<PermissionScope>("PermissionScope");
         }
 
         public Task AddAsync(PermissionScope aggregate)
         {
-            throw new NotImplementedException();
+            if (aggregate == null) throw new ArgumentNullException(nameof(aggregate));
+            if (aggregate.Id == Guid.Empty) throw new ArgumentException("", nameof(aggregate));
+            if (_collection.Find(s => s.Id == aggregate.Id).SingleOrDefault() != null) throw new InvalidOperationException();
+            return  _collection.InsertOneAsync(aggregate);
         }
 
         public Task DeleteAsync(PermissionScope aggregate)
         {
-            throw new NotImplementedException();
+            if (aggregate == null) throw new ArgumentNullException(nameof(aggregate));
+            if (aggregate.Id == Guid.Empty) throw new ArgumentException("", nameof(aggregate));
+            return _collection.DeleteOneAsync(s => s.Id == aggregate.Id);
         }
 
         public Task<PermissionScope> FindByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            if (id == Guid.Empty) throw new ArgumentNullException(nameof(id));
+            return _collection.Find(s => s.Id == id).SingleOrDefaultAsync();
         }
 
         public Task<PermissionScope> FindByScopeNameAsync(string scopeName)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(scopeName)) throw new ArgumentNullException(nameof(scopeName));
+            return _collection.Find(s => s.ScopeName == scopeName).SingleOrDefaultAsync();
         }
 
-        public Task<IQueryable<PermissionScope>> QueryAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public Task<IQueryable<PermissionScope>> QueryAllAsync() => Task.FromResult<IQueryable<PermissionScope>>(_collection.AsQueryable());
+
 
         public Task SaveAsync(PermissionScope aggregate)
         {
-            throw new NotImplementedException();
+            if (aggregate == null) throw new ArgumentNullException(nameof(aggregate));
+            if (aggregate.Id == Guid.Empty) throw new ArgumentException("", nameof(aggregate));
+            if (_collection.Find(s => s.Id == aggregate.Id).SingleOrDefault() == null) throw new InvalidOperationException();
+            return _collection.ReplaceOneAsync(s => s.Id == aggregate.Id, aggregate);
         }
     }
 }
