14dd2e10efa0dcc93ad0f0281ae491d54e7bddad
Henry Chu <HenryTech@outlook.com>
2016-08-25 11:19:13 +0800

    seprated user display name to given name and surn name

--- src/Obsidian.Domain/UserProfile.cs
+++ src/Obsidian.Domain/UserProfile.cs
@@ -10,10 +10,16 @@ namespace Obsidian.Domain
     public class UserProfile
     {
         /// <summary>
-        /// Represents the display name of a <see cref="User"/>.
+        /// Represents the first name of a <see cref="User"/>.
         /// </summary>
-        [ClaimType("http://schemas.obsidian.za-pt.org/ws/2016/08/idntity/claims/displayname")]
-        public string DisplayName { get; set; }
+        [ClaimType(ClaimTypes.GivenName)]
+        public string GivenName { get; set; }
+
+        /// <summary>
+        /// Represents the last name of a <see cref="User"/>.
+        /// </summary>
+        [ClaimType(ClaimTypes.Surname)]
+        public string SurnName { get; set; }
 
         /// <summary>
         /// Represents the Gender of a <see cref="User"/>.
