17af12e2f76b131494c69dd443d96892b14fb38a
Henry Chu <HenryTech@outlook.com>
2016-08-26 22:24:12 +0800

    added test Creation_Fail_When_User_Exists

--- test/Obsidian.Application.Test/UserManagement/CreateUserSagaTest.cs
+++ test/Obsidian.Application.Test/UserManagement/CreateUserSagaTest.cs
@@ -16,11 +16,11 @@ namespace Obsidian.Application.Test.UserManagement
         public async Task Invoke_Test()
         {
             var mockRepo = new Mock<IUserRepository>();
-
             var saga = new CreateUserSaga(mockRepo.Object);
             var userName = Guid.NewGuid().ToString();
             var password = Guid.NewGuid().ToString();
-            mockRepo.Setup(r => r.FindByUserNameAsync(It.Is<string>(n => n == userName))).Returns(Task.FromResult<User>(null));
+            mockRepo.Setup(r => r.FindByUserNameAsync(It.Is<string>(n => n == userName)))
+                .Returns(Task.FromResult<User>(null));
             var command = new CreateUserCommand
             {
                 UserName = userName,
@@ -29,6 +29,26 @@ namespace Obsidian.Application.Test.UserManagement
             var result = await saga.StartAsync(command);
             mockRepo.Verify(repo => repo.AddAsync(It.Is<User>(u => u.UserName == userName && u.VaildatePassword(password))));
             Assert.True(result.Succeed);
+            Assert.True(saga.IsCompleted);
+        }
+
+        [Fact]
+        public async Task Creation_Fail_When_User_Exists()
+        {
+            var mockRepo = new Mock<IUserRepository>();
+            var saga = new CreateUserSaga(mockRepo.Object);
+            var userName = Guid.NewGuid().ToString();
+            var password = Guid.NewGuid().ToString();
+            mockRepo.Setup(r => r.FindByUserNameAsync(It.Is<string>(n => n == userName)))
+                .Returns(Task.FromResult<User>(User.Create(Guid.NewGuid(), userName)));
+            var command = new CreateUserCommand
+            {
+                UserName = userName,
+                Password = password
+            };
+            var result = await saga.StartAsync(command);
+            Assert.False(result.Succeed);
+            Assert.True(saga.IsCompleted);
         }
     }
 }
