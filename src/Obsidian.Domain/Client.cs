66fe75a5d43d4e281045edc10b58d94e4a75a981
SamLangTen <samlangten@outlook.com>
2016-08-28 15:13:15 +0800

    added client creation saga

--- src/Obsidian.Domain/Client.cs
+++ src/Obsidian.Domain/Client.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using Obsidian.Domain.Shared;
-
 namespace Obsidian.Domain
 {
     public class Client : IEntity, IAggregateRoot
     {
-        public static Client Create(Guid id, string secret, string redirectUri)
-             => new Client { Id = id, Secret = secret, RedirectUri = new Uri(redirectUri) };
+        public static Client Create(Guid id, string redirectUri)
+             => new Client { Id = id, RedirectUri = new Uri(redirectUri) };
 
         public Guid Id { get; private set; }
         public Uri RedirectUri { get; set; }
         public string Secret { get; private set; }
         public string DisplayName { get; set; }
+
+        public void UpdateSecret() => Secret = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+
     }
 }
