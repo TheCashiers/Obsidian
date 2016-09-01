37a055a7fdc270c485afcceaecd8acb3f1450b8e
SamLangTen <samlangten@outlook.com>
2016-09-01 17:53:16 +0800

    added clientscontroller creation api

--- src/Obsidian.Application/DependencyInjection/SagaBusServiceCollectionExtensions.cs
+++ src/Obsidian.Application/DependencyInjection/SagaBusServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Obsidian.Application.ClientManagement;
 using Obsidian.Application.OAuth20;
 using Obsidian.Application.ProcessManagement;
 using Obsidian.Application.UserManagement;
@@ -21,6 +22,7 @@ namespace Obsidian.Application.DependencyInjection
         /// <returns>The same service collection so that multiple calls can be chained.</returns>
         public static IServiceCollection AddSaga(this IServiceCollection services)
             => services.AddTransient<OAuth20Saga>()
-                       .AddTransient<CreateUserSaga>();
+                       .AddTransient<CreateUserSaga>()
+                       .AddTransient<CreateClientSaga>();
     }
 }
--- src/Obsidian/Controllers/ApiControllers/ClientsController.cs
+++ src/Obsidian/Controllers/ApiControllers/ClientsController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
+using Obsidian.Application.ClientManagement;
+using Obsidian.Application.Dto;
+using Obsidian.Application.ProcessManagement;
 using Obsidian.Domain.Repositories;
 using Obsidian.QueryModel.Mapping;
 using System;
@@ -13,11 +16,13 @@ namespace Obsidian.Controllers.ApiControllers
     [Route("api/[controller]")]
     public class ClientsController : Controller
     {
+        private readonly SagaBus _sagaBus;
         private readonly IClientRepository _clientRepository;
 
-        public ClientsController(IClientRepository repo)
+        public ClientsController(IClientRepository repo, SagaBus bus)
         {
             _clientRepository = repo;
+            _sagaBus = bus;
         }
 
         [HttpGet]
@@ -36,6 +41,18 @@ namespace Obsidian.Controllers.ApiControllers
                 return NotFound();
             }
             return Ok(Mapper.Map<QueryModel.Client>(client));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ClientCreationDto dto)
+        {
+            var cmd = new CreateClientCommand { DisplayName = dto.DisplayName, RedirectUri = dto.RedirectUri };
+            var result = await _sagaBus.InvokeAsync<CreateClientCommand, ClientCreationResult>(cmd);
+            if (result.Succeed)
+            {
+                return Created(Url.Action(nameof(GetById),new { id = result.Id  }),null);
+            }
+            return StatusCode(412, result.Message);
         }
     }
 }
