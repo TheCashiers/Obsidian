afa2c6f782abad853e50aedcc111b1de3fef93b0
SamLangTen <samlangten@outlook.com>
2016-09-01 18:34:30 +0800

    added scope create & query api

---
+++ src/Obsidian.QueryModel/PermissionScope.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Obsidian.QueryModel
+{
+    public class PermissionScope
+    {
+        public Guid Id { get; private set; }
+
+        public string ScopeName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+        public IList<string> ClaimTypes { get; set; }
+    }
+}
---
+++ src/Obsidian/Controllers/ApiControllers/ScopesController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Obsidian.Application.Dto;
+using Obsidian.Application.ProcessManagement;
+using Obsidian.Application.ScopeManagement;
+using Obsidian.Domain.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace Obsidian.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    public class ScopesController : Controller
+    {
+        private readonly SagaBus _sagaBus;
+        private readonly IPermissionScopeRepository _scopeRepository;
+
+        public ScopesController(IPermissionScopeRepository scopeRepo, SagaBus bus)
+        {
+            _scopeRepository = scopeRepo;
+            _sagaBus = bus;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = await _scopeRepository.QueryAllAsync();
+            return Ok(query.ProjectTo<QueryModel.PermissionScope>());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var scope = await _scopeRepository.FindByIdAsync(id);
+            if (scope == null)
+            {
+                return NotFound();
+            }
+            return Ok(Mapper.Map<QueryModel.PermissionScope>(scope));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]ScopeCreationDto dto)
+        {
+            var cmd = new CreateScopeCommand
+            {
+                ClaimTypes = dto.ClaimTypes,
+                Description = dto.Description,
+                DisplayName = dto.DisplayName,
+                ScopeName = dto.ScopeName
+            };
+
+
+            var result = await _sagaBus.InvokeAsync<CreateScopeCommand, ScopeCreationResult>(cmd);
+            if (result.Succeed)
+            {
+                return Created(Url.Action(nameof(GetById), new { id = result.Id }), null);
+            }
+            return StatusCode(412, result.Message);
+        }
+    }
+}
