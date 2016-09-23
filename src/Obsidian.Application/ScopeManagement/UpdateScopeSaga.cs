c28d84141e13e1f8b453a9234ef193fd101b033b
SamLangTen <samlangten@outlook.com>
2016-09-23 22:46:13 +0800

    added update scope info saga

---
+++ src/Obsidian.Application/ScopeManagement/UpdateScopeSaga.cs
@@ -0,0 +1,43 @@
+﻿using Obsidian.Application.ProcessManagement;
+using Obsidian.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Obsidian.Application.ScopeManagement
+{
+    public class UpdateScopeSaga : Saga, IStartsWith<UpdateScopeInfoCommand, MessageResult<UpdateScopeInfoCommand>>
+    {
+
+        private bool _isCompleted;
+
+        private readonly IPermissionScopeRepository _repo;
+
+        public UpdateScopeSaga(IPermissionScopeRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<MessageResult<UpdateScopeInfoCommand>> StartAsync(UpdateScopeInfoCommand command)
+        {
+            _isCompleted = true;
+            var scope = await _repo.FindByIdAsync(command.Id);
+            if (scope == null)
+                return new MessageResult<UpdateScopeInfoCommand> {
+                       Succeed = false,
+                       Message = $"PermissionScope of Id {command.Id} doesn't exist."
+                };
+            //update
+            scope.Description = command.Description;
+            scope.DisplayName = command.DisplayName;
+            await _repo.SaveAsync(scope);
+            return new MessageResult<UpdateScopeInfoCommand> {
+                Succeed = true,
+                Message = $"Information of PermissionScope {scope.Id} changed."
+            };
+        }
+
+        protected override bool IsProcessCompleted() => _isCompleted;
+
+    }
+}
