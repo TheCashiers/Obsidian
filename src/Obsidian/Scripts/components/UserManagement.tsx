c89e0325a694e5d0c78814b08bd74f2cb5ada103
Cubelrti <tankrity@gmail.com>
2016-09-28 22:18:00 +0800

    changed user management style

--- src/Obsidian/Scripts/components/UserManagement.tsx
+++ src/Obsidian/Scripts/components/UserManagement.tsx
@@ -2,42 +2,42 @@
 import { Link } from "react-router";
 
 const UserItem = (props) => (
-    <li>
-        {props.username}
+<div>
+
+
+
+<div className="list-group-item">
+    <div className="row-action-primary">
+      <i className="fa fa-user"></i>
+    </div>
+    <div className="row-content">
+      <div className="least-content">
         <Link to="/manage/users/edit/" query={{username:props.username,id:props.id}}>
             <button className="btn btn-lg btn-primary btn-raised">
                 Edit
             </button>    
         </Link>
-    </li>
+      </div>
+      <h4 className="list-group-item-heading">{props.username}</h4>
+
+      <p className="list-group-item-text">{props.id}</p>
+    </div>
+  </div>
+  <div className="list-group-separator"></div>
+</div>
 );
 
 export const UserList = (props) => (
     <div className="content-wrapper content">
 
-        <div className="list-group-item">
-            <div className="row-action-primary">
-                <i className="fa fa-user"/>
-            </div>
-            <div className="row-content">
-                <div className="action-secondary"><i className="fa fa-user"/></div>
-                <h4 className="list-group-item-heading">Tile with an icon</h4>
-                <p className="list-group-item-text">Donec id elit non mi porta gravida at eget metus.</p>
-            </div>
+        <div className="list-group">
+  {props.users.map((user, index) => <UserItem username={user.userName} id={user.id} key={user.id}/>) }
         </div>
-        <div className="list-group-separator"></div>
 
 
 
     
-        <div className="box box-solid box-info">
-            <div className="box-header">
-                <h3 className="box-title">Users</h3>
-            </div>
-            <div className="box-body">
-            <ul>{props.users.map((user, index) => <UserItem username={user.userName} id={user.id} key={user.id}/>) }</ul>
-            </div>
-        </div>
+        
         <Link to="/manage/users/create">
             <button className="btn btn-lg btn-success">Create User</button>
         </Link>
