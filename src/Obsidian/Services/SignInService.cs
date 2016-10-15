3e73ce886b7c13482abe78227e5133460d4fc6be
Henry Chu <henrytech@outlook.com>
2016-10-16 07:41:38 +0800

    added SigninService

---
+++ src/Obsidian/Services/SignInService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Obsidian.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Obsidian.Services
+{
+    public class SignInService
+    {
+        private readonly HttpContext _httpContext;
+
+        public SignInService(IHttpContextAccessor accessor)
+        {
+            _httpContext = accessor.HttpContext;
+        }
+
+        public async Task CookieSignInAsync(User user, IList<PermissionScope> scopes)
+        {
+            
+        }
+
+        public async Task CookieSignOutAsync(User user)
+        {
+
+        }
+    }
+}
