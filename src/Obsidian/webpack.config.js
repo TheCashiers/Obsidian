bc47bd81c1cdc06b036b84e4be4e8b38599906da
Cubelrti <tankrity@gmail.com>
2017-02-03 10:35:44 +0800

    fix incompatible webpack settings
    run "npm update webpack -g" if not using webpack 2.2.1

--- src/Obsidian/webpack.config.js
+++ src/Obsidian/webpack.config.js
@@ -11,18 +11,14 @@
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js",".css"]
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js",".css"]
     },
 
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { test: /\.tsx?$/, loader: "ts-loader" },
-            { test: /\.css$/, loader:"style-loader" }
-        ],
-
-        Loaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.css$/, loader:"style-loader" },
             { test: /\.js$/, loader: "source-map-loader", enforce: 'pre' }
         ]
     },
