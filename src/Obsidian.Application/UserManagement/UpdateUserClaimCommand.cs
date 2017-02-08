83f5aadecbbcd8b6e06f264950e6f0276d247642
SamLangTen <samlangten@outlook.com>
2017-02-08 14:40:10 +0800

    Add: user claims modify api for mapi

---
+++ src/Obsidian.Application/UserManagement/UpdateUserClaimCommand.cs
@@ -0,0 +1,15 @@
+﻿using Obsidian.Application.ProcessManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Obsidian.Application.UserManagement
+{
+    public class UpdateUserClaimCommand : Command<MessageResult>
+    {
+        public Guid UserId { get; set; }
+        public IDictionary<string,string> Claims { get; set; }
+    }
+}
