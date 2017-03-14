a1db70afb624360cff1a6ddf1cd5c09d64e59805
SamLangTen <samlangten@outlook.com>
2017-03-14 12:54:08 +0800

    add getusername account api

--- src/Obsidian/Controllers/ApiControllers/AccountController.cs
+++ src/Obsidian/Controllers/ApiControllers/AccountController.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Obsidian.Domain.Repositories;
 namespace Obsidian.Controllers.ApiControllers
 {
     [Route("api/[controller]")]
     public class AccountController : Controller
     {
 
-        [Authorize(Policy = "NameIdentifierPolicy", ActiveAuthenticationSchemes = "Bearer")]
-        [HttpGet]
-        [Route("Id")]
-        public Task<ActionResult> GetUserId()
+        private readonly IUserRepository _userRepository;
+
+        public AccountController(IUserRepository repo)
         {
-            throw new NotImplementedException();
+            _userRepository = repo;
+        }
+
+        [Authorize(Policy = "GetUsernamePolicy", ActiveAuthenticationSchemes = "Bearer")]
+        [HttpGet]
+        [Route("Username")]
+        public async Task<ActionResult> GetUserName()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var user = await _userRepository.FindByIdAsync(new Guid(userId));
+            return Ok(new { Username = user.UserName });
         }
 
     }
