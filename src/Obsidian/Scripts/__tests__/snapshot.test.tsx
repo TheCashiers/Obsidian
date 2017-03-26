669c9069608d91da88dea710d93bbc2b63fd3a78
Cubelrti <tankrity@gmail.com>
2017-03-26 18:57:34 +0800

    fixing ci

--- src/Obsidian/Scripts/__tests__/snapshot.test.tsx
+++ src/Obsidian/Scripts/__tests__/snapshot.test.tsx
@@ -1,18 +1,37 @@
 import * as React from "react";
 import { ClientList } from "../components/ClientManagement"
-import { MaterialForm } from "../components/Form"
+import { MaterialForm, MaterialInput } from "../components/Form"
 import { create } from 'react-test-renderer';
+import { PortalHeader, UserInfo } from "../components/PortalElements";
+
 
 it('ClientList render snapshot', () => {
     const clientList = create(
-        <ClientList clients={[{ displayName:"foo",id:"bar"}]} />
+        <ClientList clients={[
+            { displayName: "foo", id: "bar" },
+            { displayName: "foo1", id: "bar1" }
+            ]} />
     ).toJSON();
     expect(clientList).toMatchSnapshot();
 });
 
 it("MaterialForm render snapshot", () => {
     const materialForm = create(
-        <MaterialForm action="Create sth" origin="../foo" onSubmit={(e)=>e.preventDefault()}/>
+        <MaterialForm action="Create sth" origin="../foo" onSubmit={(e) => e.preventDefault()} >
+            <MaterialInput name="username"
+                label="Username"
+                onInputChange={new Function()}
+                value="foo"
+                placeholder="Username..."
+                type="text" />
+            <MaterialInput name="redirectUri"
+                label="Redirect Uri"
+                onInputChange={new Function()}
+                value="http://bar.com"
+                placeholder="http://example.com"
+                type="text" />
+        </MaterialForm>
+        
     ).toJSON();
     expect(materialForm).toMatchSnapshot();
-})
+});
