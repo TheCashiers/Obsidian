3c931ab7229fddc91297fff507babb02a69a759e
Cubelrti <tankrity@gmail.com>
2017-03-26 10:57:30 +0800

    add prefetching in webpack

--- src/Obsidian/webpack.config.js
+++ src/Obsidian/webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.PrefetchPlugin("axios/index.js"),
         new webpack.optimize.CommonsChunkPlugin({
             name: ['vendor','styles']
         }),
