1619028b79459e4dcb7eab34a3813f8574ef33a4
Henry Chu <henrytech@outlook.com>
2017-03-30 13:08:43 +0800

    fixed nvm loading

--- travis.sh
+++ travis.sh
@@ -9,6 +9,8 @@ sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
 sudo apt-get install -y yarn dotnet-dev-1.0.1 mongodb-org
 
