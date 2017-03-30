c1da23afb77b080a06312491a867b3af9ab9c085
Henry Chu <henrytech@outlook.com>
2017-03-30 11:20:07 +0800

    use same client for mongo test updated nuget package

--- test/Obsidian.Persistence.Test/Repositories/MongoRepositoryTest.cs
+++ test/Obsidian.Persistence.Test/Repositories/MongoRepositoryTest.cs
@@ -8,13 +8,10 @@ namespace Obsidian.Persistence.Test.Repositories
     {
         private const string dbUri = "mongodb://127.0.0.1:27017";
         private readonly string testDbName = "ObsidianTest_" + Guid.NewGuid();
+        private IMongoClient client = new MongoClient(dbUri);
 
-        protected IMongoDatabase Database => new MongoClient(dbUri).GetDatabase(testDbName);
+        protected IMongoDatabase Database => client.GetDatabase(testDbName);
 
-        protected override void CleanupDatabase()
-        {
-            var client = new MongoClient(dbUri);
-            client.DropDatabase(testDbName);
-        }
+        protected override void CleanupDatabase() => client.DropDatabase(testDbName);
     }
 }
