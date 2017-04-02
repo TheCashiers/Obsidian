32861ecdf6977cc1e02e55b31a73db83d40dab4d
Henry Chu <henrytech@outlook.com>
2017-04-02 23:29:42 +0800

    try update ci config

--- travis.sh
+++ travis.sh
@@ -13,13 +13,13 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
 sudo apt-get install -y yarn dotnet-dev-1.0.1 mongodb-org
+sudo service restart mongod
 
 dotnet restore
 cd ./src/Obsidian/
 yarn
 dotnet build
 
-sudo service start mongod
 cd ../../test
 for d in ./*/ ; 
 do
