b3b8eb2c57b7f97b3bb3fb6c234a8dedafe45f1a
Henry Chu <henrytech@outlook.com>
2017-05-01 17:36:12 +0800

    updated build script for windows

--- build.ps1
+++ build.ps1
@@ -1,21 +1,40 @@
-Write-Host "========== Obsidian Build Script for Windows ==========" -ForegroundColor Cyan
-
-$startTime = Get-Date
-Write-Host "========== Start Configuring Package Dependencies ==========" -ForegroundColor Cyan
-Start-Process dotnet -ArgumentList restore -NoNewWindow -Wait
-Start-Process yarn -WorkingDirectory ".\src\Obsidian" -Wait -NoNewWindow
-[Console]::ResetColor()
-
-Write-Host "========== Start Building ==========" -ForegroundColor Cyan
-Start-Process dotnet -ArgumentList build -WorkingDirectory ".\src\Obsidian" -NoNewWindow -Wait
-
-Write-Host "========== Start Testing ==========" -ForegroundColor Cyan
-$testProjDirs = Get-ChildItem -Path .\test\ -Directory
-foreach($d in $testProjDirs){
-	Start-Process dotnet -ArgumentList test -WorkingDirectory $d.FullName -NoNewWindow -Wait
+function Print-Section ([String]$Content){
+    Write-Host "============= $($Content) =============" -ForegroundColor Cyan
 }
 
-Start-Process npm -ArgumentList "run test:cover" -WorkingDirectory ".\src\Obsidian" -Wait -NoNewWindow
-[Console]::ResetColor()
+function Run-BuildTask{ 
+param([String]$Command, 
+      [String[]]$Arguments, 
+      [String]$Dir)
+    if($Arguments.Count -eq 0){
+        Start-Process $Command -WorkingDirectory $Dir -NoNewWindow -Wait
+    }
+    else{
+        Start-Process $Command -ArgumentList $Arguments -WorkingDirectory $Dir -NoNewWindow -Wait
+    }
+    [Console]::ResetColor()
+}
+
+Print-Section "Obsidian Build Script for Windows"
+
+$webProjectDir = [System.IO.Path]::Combine( $PSScriptRoot ,"src","Obsidian")
+$testDir =  [System.IO.Path]::Combine( $PSScriptRoot ,"test")
+$startTime = Get-Date
+
+Print-Section "Start Configuring Package Dependencies (Stage 1 of 3)"
+Run-BuildTask dotnet -Arguments restore -Dir $PSScriptRoot
+Run-BuildTask yarn -Dir $webProjectDir
+
+Print-Section "Start Building (Stage 2 of 3)"
+Run-BuildTask dotnet -Arguments build -Dir $webProjectDir
+
+Print-Section "Start Testing (Stage 3 of 3)"
+$testProjDirs = Get-ChildItem -Path $testDir -Directory
+foreach($d in $testProjDirs){
+	Run-BuildTask dotnet -Arguments test -Dir $d.FullName
+}
+
+Run-BuildTask npm -Arguments "run test:cover" -Dir $webProjectDir
+
 $endTime = Get-Date
-Write-Host "========== Obsidian Build Script Finished in $($endTime-$startTime) ==========" -ForegroundColor Cyan
+Print-Section "Obsidian Build Script Finished in $($endTime-$startTime)"
