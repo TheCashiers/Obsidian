03de323126db1b1f0897d120660c65464d1c5a30
Henry Chu <henrycobaltech@outlook.com>
2017-07-04 18:54:55 +0800

    chenged node version from 8 to 6 in install script

--- build.cent.sh
+++ build.cent.sh
@@ -1,18 +1,15 @@
 sudo cp ./mongodb-org-3.4.repo /etc/yum.repos.d/mongodb-org-3.4.repo
 sudo yum install -y mongodb-org wget curl libunwind libicu
 
-# install nodejs
-sudo curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
-
 # install dotnet
 curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?linkid=848821
 sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
 sudo ln -s /opt/dotnet/dotnet /usr/local/bin
 
-# install yarn
+# install yarn and nodejs
 sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-sudo yum install yarn
+sudo yum install nodejs yarn
 
 sudo service mongod start
 
