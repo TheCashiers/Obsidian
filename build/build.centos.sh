975fe3682fd5f90ace6754f99192a1945ac714ee
Henry Chu <henrycobaltech@outlook.com>
2017-07-04 19:31:26 +0800

    updated build script location

---
+++ build/build.centos.sh
@@ -0,0 +1,27 @@
+echo "=============== Obsidian Build Script for CentOS ============="
+echo "=============== Cloning git repository =================="
+sudo yum install -y curl wget git
+git clone https://github.com/ZA-PT/Obsidian.git
+cd Obsidian
+
+echo "=============== Configuring environment =================="
+sudo cp ./mongodb-org-3.4.repo /etc/yum.repos.d/mongodb-org-3.4.repo
+
+sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source ~/.bashrc
+nvm install node
+source ~/.bashrc
+sudo yum install -y yarn mongodb-org
+
+curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?linkid=848821
+sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+rm dotnet.tar.gz
+
+echo "=============== Starting database service ===================="
+sudo service mongod start
+
+echo "=============== Starting build ====================="
+chmod +x ./build.core.sh
+./build.core.sh
