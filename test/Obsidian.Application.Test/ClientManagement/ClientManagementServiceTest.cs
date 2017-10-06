1fad89f5814ec1d7d82d27bd1332e2eb6ab840cd
Henry Chu <henrycobaltech@outlook.com>
2017-10-07 01:04:33 +0800

    added one test for client menagament service

---
+++ test/Obsidian.Application.Test/ClientManagement/ClientManagementServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using Obsidian.Application.ClientManagement;
+using Obsidian.Domain;
+using Obsidian.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Obsidian.Application.Test.ClientManagement
+{
+    public class ClientManagementServiceTest
+    {
+        [Fact]
+        public async Task CreateTest()
+        {
+            var mockRepo = new Mock<IClientRepository>();
+            var service = new ClientService(mockRepo.Object);
+            await service.CreateClient("xxx", "http://xxx.com");
+            mockRepo.Verify(r => r.AddAsync(It.IsAny<Client>()));
+        }
+
+    }
+}
