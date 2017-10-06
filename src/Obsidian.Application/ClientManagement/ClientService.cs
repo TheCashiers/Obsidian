392f13d389afe8dfdcea2fd431824df4fe871e0e
SamLangTen <samlangten@outlook.com>
2017-10-07 00:12:42 +0800

    remove sage in client

---
+++ src/Obsidian.Application/ClientManagement/ClientService.cs
@@ -0,0 +1,44 @@
+﻿using Obsidian.Domain;
+using Obsidian.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsidian.Application.ClientManagement
+{
+    public class ClientService
+    {
+        private readonly IClientRepository _repo;
+        public ClientService(IClientRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Client> CreateClient(string displayName,string redirectUri)
+        {
+            var client = Client.Create(Guid.NewGuid(), redirectUri);
+            client.DisplayName = displayName;
+            client.UpdateSecret();
+            await _repo.AddAsync(client);
+            return client;
+        }
+
+        public async Task<Client> UpdateClientSecret(Guid clientId)
+        {
+            var client = await _repo.FindByIdAsync(clientId);
+            client.UpdateSecret();
+            await _repo.SaveAsync(client);
+            return client;
+        }
+
+        public async Task<Client> UpdateClient(Guid clientId,string displayName,string redirectUri)
+        {
+            var client = await _repo.FindByIdAsync(clientId);
+            client.DisplayName = displayName;
+            client.RedirectUri = new Uri(redirectUri);
+            await _repo.SaveAsync(client);
+            return client;
+        }
+}
+}
