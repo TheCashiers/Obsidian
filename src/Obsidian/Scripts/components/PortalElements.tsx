bb25bb76e8834ab973063882826a4c55b652cc9a
Cubelrti <tankrity@gmail.com>
2017-10-10 10:12:48 +0800

    remove unused code

--- src/Obsidian/Scripts/components/PortalElements.tsx
+++ src/Obsidian/Scripts/components/PortalElements.tsx
@@ -102,25 +102,6 @@ export const PortalHeader = (props) => (
                         <li><Link to="/manage/users">Users</Link></li>
                         <li><Link to="/manage/clients">Clients</Link></li>
                         <li><Link to="/manage/scopes">Scopes</Link></li>
-                        <li className="dropdown">
-                            <a
-                                href="#"
-                                className="dropdown-toggle"
-                                data-toggle="dropdown"
-                            >
-                                Dropdown
-                                <span className="caret"/>
-                            </a>
-                            <ul className="dropdown-menu" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li className="divider"/>
-                                <li><a href="#">Separated link</a></li>
-                                <li className="divider"/>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
                     </ul>
                     <SearchBox handleFilterChange={props.handleFilterChange} filter={props.filter}/>
                     <ul className="nav navbar-nav navbar-right">
